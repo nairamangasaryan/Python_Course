d45a85836d134ac8faaeefe7216a9b4bfe91585c
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-06 01:52:46 +0400

    Added the 1-st lesson content

---
+++ week1/lesson1.py
@@ -0,0 +1,18 @@
+print()
+print("We start PYTHON course")
+print()
+print("Today our 1-st lesson")
+
+# Main topics for 1-st lesson
+print(''' 
+1. Itroduction of the course
+2. About Python
+3. Types of programming leanguages
+4. Python is an "Interpreating" language
+5. iPython and Python scripts
+6. How to isntall the Python
+7. Visual Studio Code. Installing VS Code
+8. Extensions of VS Code
+9. Installation of "Andromeda", "Code Runner", "File Utiles", "File-icons", "Python" extensions
+''')
+print()
