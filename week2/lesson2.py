22de093c66ea348019a9c0c70b3d92df23b4d2a2
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-06 02:27:57 +0400

    Content of 2-nd lesson

---
+++ week2/lesson2.py
@@ -0,0 +1,16 @@
+# Main topics for 2-nd lesson
+print()
+print("""
+SECOND LESSON
+
+1. Structure of VSC
+2. View --> Command Palette (or Fn + F1)
+3. Terminal of VSC
+4. How to create New File, New Folder
+5. Files for Python: .py
+6. Command 'Print()'
+7. First programm in Python
+8. Zen of Python
+9. Programm with using of 'Theorema of Phytagoras"
+""")
+print()
---
+++ week2/phytagoras.py
@@ -0,0 +1,6 @@
+# Theorema of Phytagoras
+x = 3
+y = 4
+z = (x ** 2 + y ** 2) ** 0.5
+print("z = ",z)
+print(5.0 == 5)
