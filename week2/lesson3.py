da4238da32d8edbc9d5f7be3fcef19fde2f0b369
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-08 02:00:46 +0400

    Added main Git commands

--- week2/lesson3.py
+++ week2/lesson3.py
@@ -5,6 +5,24 @@ print("""
 
 1. What is Git
 2. Instalation of Git
-3. What is GitHub
-4. Creating account in 'github.com'
+3. Main commands of Git
+    - git config --global
+    - git init
+    - git status
+    - git add
+    - git rm --cached
+    - git commit -m 'Comment'
+    - git commit -a -m 'Camment'
+    - git diff
+    - git rm
+    - git restore
+    - git mv
+    - git log
+    - git branch 'name'
+    - git branch
+    - git switch 'name'
+    - git marge -m 'Comment' name
+    
+4. What is GitHub
+5. Creating account in 'github.com'
 """)
