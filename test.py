297ee9828fd93f410eb9cac35a40d37ca355c9e0
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-10 00:31:27 +0400

    modified version of test.py file from main branch

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 #This file is for testing "git pull" command
 print("'git pull' command is working correct")
+print("This file is 'pulled' from github 'main' branch by using the command: 'git pull ogigin main'")
