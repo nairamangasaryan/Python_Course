0b1d5cf6f1a92e2b654c3ef831b59962f9a93056
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-11 19:22:40 +0400

    Added info about git pull,git push, git checkout, git branch

---
+++ week3/Lesson4.py
@@ -0,0 +1,12 @@
+print()
+print("Below are some commands using in Git and GitHub")
+print("""
+    - git checkout -b branch_name  #  create a new breanch and switch to that branch
+    - git branch branchname # create a new breanch
+    - git branch # shows all branches, and mentions current branch by *
+    
+    - git pull # this command is used to fetch and download content from a remote repo and immediately update the local repo to match that content.
+    - git push # this command is used to upload local repo content to a remote repo
+    - git push --all # push all branches from local repo into remote repo
+    - git checkout <Commit hash> # reset the commit with mentioned hash number
+""")
